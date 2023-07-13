58154e9fa5f15fb04b06153ae0f207057d3a8155
AbenezerM <abenezermulugeta41@gmail.com>
2023-07-12 21:35:21 -0400

    Add security components, bootstrap and create routes for the security features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { ResetpasswordComponent } from './component/resetpassword/resetpassword.component';
+import { VerifyComponent } from './component/verify/verify.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'reset-password', component: ResetpasswordComponent },
+  { path: 'users/verify/account/:key', component: VerifyComponent },
+  { path: 'users/verify/password/:key', component: VerifyComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
